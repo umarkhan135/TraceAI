5f3abe4724ccb7a0ce20f53a51aa369157ddf1e4
Umar Khan <1khanumar@gmail.com>
2026-01-11 01:53:29 -0500

    walkghtouh

---
+++ multiply.py
@@ -0,0 +1,12 @@
+def multiply(a, b):
+    """
+    Multiply two numbers and return the result.
+
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        The product of a and b
+    """
+    return a * b
